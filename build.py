c876b6efd7e001539844dc7f8d32e509c8500597
Aaron <aaron@sajatypeworks.com>
2019-11-12 01:10:24 -0800

    Updated Cyrillic and other fixes

--- build.py
+++ build.py
@@ -38,13 +38,36 @@ if __name__ == "__main__":
         file_pathPL = OUTPUT_DIR / "CascadiaPL.ttf"
 
         # 4. Creating NerdFont variants
-        instanceNF = generator.generate_instance(instance_descriptor)
+        instanceNF = generator.generate_instance(instance_descriptor)  #rebuilding these instead of as a deepcopy avoids an error when compiling the TTF
         instancePL = generator.generate_instance(instance_descriptor)
+
+
+        # 4.1 Modifying some attributes to correct the metadata
+        setattr(instance.info,"openTypeOS2Panose",[2,11,6,9,2,0,0,2,0,4])
+        setattr(instanceNF.info,"openTypeOS2Panose",[2,11,6,9,2,0,0,2,0,4])
+        setattr(instancePL.info,"openTypeOS2Panose",[2,11,6,9,2,0,0,2,0,4])
+        
+        setattr(instance.info,"openTypeHheaAscender",instance.info.openTypeOS2TypoAscender)
+        setattr(instance.info,"openTypeHheaDescender",instance.info.openTypeOS2TypoDescender)
+        setattr(instance.info,"openTypeHheaLineGap",instance.info.openTypeOS2TypoLineGap)
+
+        setattr(instanceNF.info,"openTypeHheaAscender",instanceNF.info.openTypeOS2TypoAscender)
+        setattr(instanceNF.info,"openTypeHheaDescender",instanceNF.info.openTypeOS2TypoDescender)
+        setattr(instanceNF.info,"openTypeHheaLineGap",instanceNF.info.openTypeOS2TypoLineGap)
+
+        setattr(instancePL.info,"openTypeHheaAscender",instancePL.info.openTypeOS2TypoAscender)
+        setattr(instancePL.info,"openTypeHheaDescender",instancePL.info.openTypeOS2TypoDescender)
+        setattr(instancePL.info,"openTypeHheaLineGap",instancePL.info.openTypeOS2TypoLineGap)
+
+        setattr(instanceNF.info, "familyName","Cascadia Code NF")
+        setattr(instancePL.info, "familyName","Cascadia Code PL")
+
+        #4.2 GET BACK TO WORK
 
         NF_UFO = ufoLib2.objects.font.Font.open(INPUT_DIR / "nerdfonts" / "NerdfontsNF.ufo")
         PL_UFO = ufoLib2.objects.font.Font.open(INPUT_DIR / "nerdfonts" / "NerdfontsPL.ufo")
         
-        # Adding glyphs
+        #4.5 Adding glyphs
         print ("Adding Nerd Font glyphs")
         for glyph in NF_UFO.glyphOrder:
             if glyph in instanceNF.glyphOrder:
@@ -70,6 +93,10 @@ if __name__ == "__main__":
         setattr(instance_noLIG.features,"text",noLIG_fea)
         setattr(instanceNF_noLIG.features,"text",noLIG_fea)
         setattr(instancePL_noLIG.features,"text",noLIG_fea)
+
+        setattr(instance_noLIG.info, "familyName","Cascadia Code N")
+        setattr(instanceNF_noLIG.info, "familyName","Cascadia Code NLN")
+        setattr(instancePL_noLIG.info, "familyName","Cascadia Code PLN")
 
         # 6. Compile all TTFs
         print ("Compiling")
