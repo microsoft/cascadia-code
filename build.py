00f3fd489733e2e3249927abbeae836fd0b218fd
Aaron <aaronbell@users.noreply.github.com>
2021-02-08 17:13:37 -0800

    February 2021 update (#402)
    This is a significant update to Cascadia Code including a large number
    of bug fixes as well as updating the font to offer support for Fira
    Code v5 ligature support. 
    
    This update supersedes PR #373.
    
    Closes #262 - ⏎ added
    Closes #264 - additional codepoints for control characters added
    Closes #281 - `!:` and `!.` added
    Closes #290 - `/\` and `\/` added
    Closes #301 - `??=` added
    Closes #324 - ℞ added
    Closes #327 - `<:>` and other variants implemented via the `calt`
      refactoring
    Closes #359 - house added
    Closes #371 - Added x-height instruction into ttfautohint to control the
      height of the lowercase.  
    Closes #375 - Completely redesigned quote marks for better recognition
    Closes #377 - updated hinting to achieve more consistent results
    Closes #381 - increased height of thetamod
    Closes #382 - reduced the width of the hooklefts
    Closes #383 - updated heights on esh, glottalstop, glottalstopreversed
    Closes #384 - tweaked hinting a little bit. Maybe it'll help :)
    Closes #386 - added remaining soft-dotting
    Closes #392 - changed designs of angled quotes (they are now round)
    Closes #394 - changed former `~=` symbol to a simpler component-based
      version. Should be less confusing now for Lua / Matlab users. 
    Closes #395 - makes the underline thicker based on font weight
    Closes #400 - increased size of degree
    
    Closes #219 
    The full control pictures block has been added (u+2400 to u+2426). For
    purposes of rendering, the two letter abbreviations have been used
    instead of the standard three letter abbreviations:
    
    Additionally, ss20 includes the oft-unused graphical representations of
    these codepoints (for fun!):
    
    Closes #276 (infinite arrows)
    Full support for Fira Code's current ligature set (with a few
    exceptions). Now featuring infinite arrows!!! 
    
    This involved a full refactoring of the `calt` feature—for those
    interested, it now uses forward-looking substitutions instead of
    backward-looking substitutions and progressive substitution to reduce
    code. This also required some redesigning of the greater / lesser
    related ligatures. Please note, I have also removed all the obsolete
    ligatures now covered by the arrows code.
    
    Closes #329 
    There was a mismatch in the font's postscript naming conventions that
    was corrected. Should now render all weights in Word. **Note** there is
    apparently an additional bug in Mac Word's implementation of variable
    fonts which should be available in an update mid-Feb. 
    
    * Not listed – Reworked the hints for the mod and superscript glyphs so
      that they're bottom-up rather than top-down. This allows for better
      bottom alignments. 
    
    Aside from the above changes, this version also includes many other
    small updates including spacing, outline quality improvements, and
    fixing hinting.

--- build.py
+++ build.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 from pathlib import Path
 from typing import cast
+import xml.etree.cElementTree as ET
+import tempfile
 
 import cffsubr.__main__
 import fontmake.instantiator
@@ -18,9 +20,10 @@ import ufo2ft
 import ufoLib2
 import vttLib
 import vttLib.transfer
+from vttmisc import tsi1, tsic
 
-VERSION_YEAR_MONTH = 2009
-VERSION_DAY = 22
+VERSION_YEAR_MONTH = 2102
+VERSION_DAY = 3
 
 OUTPUT_DIR = Path("build")
 OUTPUT_OTF_DIR = OUTPUT_DIR / "otf"
@@ -30,7 +33,7 @@ OUTPUT_STATIC_OTF_DIR = OUTPUT_OTF_DIR / "static"
 OUTPUT_STATIC_TTF_DIR = OUTPUT_TTF_DIR / "static"
 OUTPUT_STATIC_WOFF2_DIR = OUTPUT_WOFF2_DIR / "static"
 INPUT_DIR = Path("sources")
-VTT_DATA_FILE = INPUT_DIR / "vtt_data" / "CascadiaCode.ttx"
+VTT_DATA_FILE = INPUT_DIR / "vtt_data" / "CascadiaCode_VTT.ttf"
 FEATURES_DIR = INPUT_DIR / "features"
 NERDFONTS_DIR = INPUT_DIR / "nerdfonts"
 
@@ -89,11 +92,6 @@ def set_overlap_flag(varfont: fontTools.ttLib.TTFont) -> fontTools.ttLib.TTFont:
             # Set OVERLAP_SIMPLE bit for simple glyphs
             glyph.flags[0] |= 0x40
 
-def manualHacks(varfont: fontTools.ttLib.TTFont) -> fontTools.ttLib.TTFont:
-    varfont["head"].flags = 0x000b
-    varfont.importXML(INPUT_DIR / "cvar.ttx")
-
-
 def prepare_fonts(
     designspace: fontTools.designspaceLib.DesignSpaceDocument, name: str
 ) -> None:
@@ -133,7 +131,6 @@ def to_woff2(source_path: Path, target_path: Path) -> None:
     font.flavor = "woff2"
     target_path.parent.mkdir(exist_ok=True, parents=True)
     font.save(target_path)
-
 
 # Build fonts
 # ****************************************************************
@@ -179,16 +176,43 @@ def compile_variable_and_save(
     statmake.lib.apply_stylespace_to_variable_font(styleSpace, varFont, {})
 
     print(f"[{familyName}] Merging VTT")
-    vttLib.transfer.merge_from_file(varFont, VTT_DATA_FILE)
+
+    font_vtt = fontTools.ttLib.TTFont(VTT_DATA_FILE)
+
+    for table in ["TSI0", "TSI1", "TSI2", "TSI3", "TSI5", "TSIC", "maxp"]:
+        varFont[table] = fontTools.ttLib.newTable(table)
+        varFont[table] = font_vtt[table]
+
+    # this will correct the OFFSET[R] commands in TSI1
+    if font_vtt.getGlyphOrder() != varFont.getGlyphOrder():
+        tsi1.fixOFFSET(varFont, font_vtt)
+        pass
+
     if vtt_compile:
         print(f"[{familyName}] Compiling VTT")
+        
+        tree = ET.ElementTree()
+        TSICfile = tempfile.NamedTemporaryFile()
+        varFont.saveXML(TSICfile.name, tables=["TSIC"])
+        tree = ET.parse(TSICfile.name)
         vttLib.compile_instructions(varFont, ship=True)
+        tsic.makeCVAR(varFont, tree)
+
+    else:
+        file_path = Path(str(file_path)[:-4]+"_VTT.ttf")
 
     set_overlap_flag(varFont)
 
     # last minute manual corrections to set things correctly
-    manualHacks(varFont)
-
+    # flag to enable proper rendering
+    # Adjusted the font full name so that it aligns with font spec, and also shows as expected!
+    # Adjusting postscript name to make room for the upcoming Italic
+    # Helping mac office generage the postscript name correctly for variable fonts
+    varFont["head"].flags = 0x000b
+    varFont["name"].setName(familyName, 4, 3, 1, 1033)
+    varFont["name"].setName(familyName.replace(" ","")+"-Roman", 6, 3, 1, 1033)
+    varFont["name"].setName("Roman", 17, 3, 1, 1033)
+    varFont["name"].setName(familyName.replace(" ","")+"Roman", 25, 3, 1, 1033)
 
     print(f"[{familyName}] Saving")
     file_path.parent.mkdir(exist_ok=True, parents=True)
@@ -250,6 +274,8 @@ def ttfautohint(path: str) -> None:
             "ttfautohint",
             "--stem-width",
             "nsn",
+            "--increase-x-height",
+            "0",
             "--reference",
             os.fspath(OUTPUT_STATIC_TTF_DIR / "CascadiaCode-Regular.ttf"),
             path,
@@ -302,7 +328,7 @@ if __name__ == "__main__":
         )
     )
     if args.mono:
-        processes.append(
+        processes.append( 
             pool.apply_async(
                 build_font_variable,
                 (
