1c6def46accb686a808730ea7c554bab97ecb664
Aaron <aaronbell@users.noreply.github.com>
2021-08-26 11:58:05 -0700

    July-August 2021 Fixes (#538)
    ## Summary of the Pull Request
    
    This includes two fixes:
    - [x] Closes #500 
    Changes the build process to use gftools.stat instead of statmake. The issue was caused by an 'unused' italic axis in the fonts, which has been removed, but statmake gets unhappy without it there. So the process has been adjusted to a more amenable build library. 
    
    - [x] Closes #552
    Adds the Hebrew language tag to the Regular to ensure that Hebrew positioning works correctly. 
    
    Also:
    Adjusts calt feature to ensure LIG substitutions properly function under rare conditions where the `ignore` activate incorrectly. I thought this had already been PR'd but seems not. 
    
    For example:
    `===]`
    First = becomes "equal_equal.liga", and ignore would activate on second =, blocking LIG from being substituted. If the liga form appears, LIG should appear.
    
    Turned back on the =>> =<<, etc. ligatures as there is a glyph for them now.

--- build.py
+++ build.py
@@ -14,8 +14,8 @@ import fontTools.designspaceLib
 import fontTools.ttLib
 import fontTools.ttLib.tables._g_l_y_f as _g_l_y_f
 import psautohint.__main__
-import statmake.classes
-import statmake.lib
+from gftools.stat import gen_stat_tables_from_config
+import yaml
 import ufo2ft
 import ufoLib2
 import vttLib
@@ -235,11 +235,6 @@ def compile_variable_and_save(
     print(f"[{familyName} {styleName}] Compiling")
     varFont = ufo2ft.compileVariableTTF(designspace, inplace=True)
 
-    print(f"[{familyName} {styleName}] Adding STAT table")
-    styleSpace = statmake.classes.Stylespace.from_file(INPUT_DIR / "STAT.plist")
-    statmake.lib.apply_stylespace_to_variable_font(styleSpace, varFont, {})
-
-
     print(f"[{familyName} {styleName}] Merging VTT")
 
     if "Italic" in styleName:
@@ -270,7 +265,6 @@ def compile_variable_and_save(
     varFont["head"].flags = 0x000b
 
     if "Regular" in styleName:
-        varFont["name"].setName(familyName.replace(" Regular",""), 4, 3, 1, 1033)
         varFont["name"].setName(familyName.replace(" ","")+"Roman", 25, 3, 1, 1033)
 
     print(f"[{familyName} {styleName}] Saving")
@@ -574,6 +568,15 @@ if __name__ == "__main__":
     for process in processes:
         process.get()
     del processes, pool
+
+    # Step 1.5: Adding STAT tables in one go
+    print ("[Cascadia Variable fonts] Fixing STAT tables")
+    fontSTAT = [fontTools.ttLib.TTFont(f) for f in list(OUTPUT_TTF_DIR.glob("*.ttf"))]
+    config = yaml.load(open(INPUT_DIR/"stat.yaml"), Loader=yaml.SafeLoader)
+    gen_stat_tables_from_config(config, fontSTAT)
+
+    for font in fontSTAT:
+        font.save(font.reader.file.name)
 
     # Stage 2: Autohint and maybe compress all the static things.
     if args.static_fonts is True:
